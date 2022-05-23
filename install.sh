208dfd0770b0d6d1a9d2d540639301abee44d481
Kirill Antipov <k.antipov@haulmont.com>
2022-05-23 16:58:44 +0300

    init

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-export HTTP_PROXY=http://jmix:jmix@54.215.128.225:8888
-export HTTPS_PROXY=http://jmix:jmix@54.215.128.225:8888
+export HTTP_PROXY=http://jmix:jmix@154.215.128.225:8888
+export HTTPS_PROXY=http://jmix:jmix@154.215.128.225:8888
 terraform init -input=false
 unset HTTP_PROXY
 unset HTTPS_PROXY
